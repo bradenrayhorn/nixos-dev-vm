3542be95ef4f6d664da9f660a53ddef93cab95ad
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2026-03-29 22:50:06 -0500

    chore: update pi

--- modules/home-manager/pi-agent/pi.nix
+++ modules/home-manager/pi-agent/pi.nix
@@ -3,18 +3,18 @@
 let
   pi-coding-agent = pkgs.buildNpmPackage rec {
     pname = "pi-coding-agent";
-    version = "0.52.9";
+    version = "0.64.0";
 
     src = pkgs.fetchurl {
       url = "https://registry.npmjs.org/@mariozechner/pi-coding-agent/-/pi-coding-agent-${version}.tgz";
-      hash = "sha256-Cp8qNYA3UMJi0L9moQYvpPasJYXiFlC/AORMXlV7LlE=";
+      hash = "sha256-SSdIyhoK9DEa0qFNK3dAsTPcOhvhJQ/w7klVCB2kqZo=";
     };
 
     postPatch = ''
       cp ${./package-lock.json} package-lock.json
     '';
 
-    npmDepsHash = "sha256-H5jMEuEKteOT0XDuapqix19JmBMCwORFc8zZ/At1RcA=";
+    npmDepsHash = "sha256-FTaTT7ssdAbbeMYKXAxbuWzwjRm2atttCCl4l+G63aU=";
 
     dontNpmBuild = true;
 
