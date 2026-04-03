b61eb9084575a033cc04b49c8748452f37ed7369
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2026-04-03 13:44:11 -0500

    feat(nvim): add groovy formatting

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -12,7 +12,7 @@ vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = true
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "rust",
+	pattern = "rust,groovy",
 	callback = function()
 		vim.opt_local.shiftwidth = 4
 		vim.opt_local.tabstop = 4
